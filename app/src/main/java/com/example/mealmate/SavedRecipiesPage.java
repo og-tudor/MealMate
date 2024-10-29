ee01efb7456732e07af094385eaab1da30de72aa
Tudor Frecus <tudor.frecus.edevize@gmail.com>
2024-10-29 18:12:04 +0200

    [dev] + added UI for LoginPage and SavedRecipiesPage

---
+++ app/src/main/java/com/example/mealmate/SavedRecipiesPage.java
@@ -0,0 +1,26 @@
+package com.example.mealmate;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SavedRecipiesPage extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.saved_recipies); // Ensure this layout exists
+
+        // Apply window insets to padding for edge-to-edge display
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
